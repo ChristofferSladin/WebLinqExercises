5fe610ce3efefa7ce9292e84011132910c1e3c92
Christoffer Sladin <102432146+ChristofferSladin@users.noreply.github.com>
2022-10-27 13:27:39 +0200

    Add project files.

---
+++ WebLinqExercises/Pages/Question.cshtml.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebLinqExercises.Data;
+
+namespace WebLinqExercises.Pages
+{
+    public class QuestionModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public QuestionModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet(string id)
+        {
+            if (id == "1")
+            {
+                ViewData["data"] = Data.Repository
+                    .GetAllMovies()
+                    .Where(movie => movie.ReleaseYear > 1990 && movie.ReleaseYear <2000)
+                    .ToList()
+                    .ToHtmlTable();
+            }
+            else if (id == "2")
+            {
+                ViewData["data"] = Data.Repository
+                    .GetAllMovies()
+                    .Where(movie => movie.Actors.Count() == 3)
+                    .ToList()
+                    .ToHtmlTable();
+            }
+            else if (id == "3")
+            {
+                ViewData["data"] = Data.Repository
+                    .GetAllMovies()
+                    .ToHtmlTable();
+            }
+            else if (id == "4")
+            {
+                ViewData["data"] = Data.Repository.GetAllActors().ToHtmlTable();
+            }
+            else if (id == "5")
+            {
+                ViewData["data"] = Data.Repository.GetAllActors()
+                    .Where(movie => movie.Name.Contains("g") || movie.Name.Contains("G"))
+                    .ToList()
+                    .ToHtmlTable();
+            }
+            else if (id == "6")
+            {
+                ViewData["data"] = Data.Repository.GetAllActors()
+                    .Where(movie => movie.Name == )
+                    .Select()
+                    .ToString()
+                    .ToHtmlTable();
+            }
+            else if (id == "7")
+            {
+                ViewData["data"] = Data.Repository.GetAllMovies().ToHtmlTable();
+            }
+            else if (id == "8")
+            {
+                ViewData["data"] = Data.Repository.GetAllMovies().ToHtmlTable();
+            }
+            else if (id == "9")
+            {
+                ViewData["data"] = Data.Repository.GetAllMovies().ToHtmlTable();
+            }
+            else if (id == "10")
+            {
+                ViewData["data"] = Data.Repository.GetAllMovies().ToHtmlTable();
+            }
+            else if (id == "11")
+            {
+                ViewData["data"] = Data.Repository.GetAllMovies().ToHtmlTable();
+            }
+
+        }
+    }
+}
